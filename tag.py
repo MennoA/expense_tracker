7773c4f6adb632a58d63f4c797779ba22edf3cf0
MennoA <fah.julien@gmail.com>
2023-12-04 21:57:43 +0100

    tag creation function

--- tag.py
+++ tag.py
@@ -7,7 +7,7 @@ from expense_tracker.auth import login_required
 from expense_tracker.db import get_db
 from expense_tracker.utils import get_tags
 
-bp = Blueprint('tag', __name__)
+bp = Blueprint('tag', __name__, url_prefix='/tags')
 
 # init
 @bp.route('/main')
@@ -15,9 +15,34 @@ bp = Blueprint('tag', __name__)
 def main():
     db = get_db()
     return render_template('tag/main.html', tags=get_tags())
-# select tag
 
 # create tag
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        name = request.form['name']
+        color = request.form['color']
+        description = request.form['description']
+        error = None
+
+        if not name:
+            error = 'Name is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO tags (name, color, user, description)'
+                ' VALUES (?, ?, ?, ?)',
+                (name, color, g.user['id'], description)
+            )
+            db.commit()
+            return redirect(url_for('tags.main'))
+
+    return render_template('tags/create.html')
+
 
 # edit tag
 
